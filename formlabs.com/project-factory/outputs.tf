5afdd52ec6b52ab345e8f26a91668c16eb9f63d6
Patrik Kozma <patrik.kozma@formlabs.com>
2022-09-13 11:06:26 +0200

    SD-1993: Create network projects

--- formlabs.com/project-factory/outputs.tf
+++ formlabs.com/project-factory/outputs.tf
@@ -1,3 +1,11 @@
 output "terrafrom_project" {
   value = module.terraform_project
 }
+
+output "prod_network_project" {
+  value = module.prod_network_project
+}
+
+output "dev_network_project" {
+  value = module.dev_network_project
+}
