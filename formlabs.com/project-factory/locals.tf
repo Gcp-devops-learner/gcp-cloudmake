7e63feabe078fb36e0d9662827a56311989ebd52
Laszlo Zeke <laszlo.zeke@formlabs.com>
2022-11-03 11:46:29 +0100

    SD-2045: Make a cloud logging playground project

--- formlabs.com/project-factory/locals.tf
+++ formlabs.com/project-factory/locals.tf
@@ -1,6 +1,8 @@
 locals {
   folder_ids = {
-    devops_prod = "417234724825"
-    devops_dev  = "80052349617"
+    devops_prod  = "417234724825"
+    devops_dev   = "80052349617"
+    systems_prod = "616766522095"
+    systems_dev  = "61195497701"
   }
 }
