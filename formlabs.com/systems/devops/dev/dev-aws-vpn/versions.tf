75a27be771c119439f43eed9899ec5f24d85109f
Patrik Kozma <patrik.kozma@formlabs.com>
2022-11-18 14:17:31 +0100

    SD-2042: Update versions

--- formlabs.com/systems/devops/dev/dev-aws-vpn/versions.tf
+++ formlabs.com/systems/devops/dev/dev-aws-vpn/versions.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = "1.3.3"
+  required_version = "1.3.5"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.42"
+      version = "~> 4.43"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.42"
+      version = "~> 4.43"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.37"
+      version = "~> 4.40"
     }
   }
 }
