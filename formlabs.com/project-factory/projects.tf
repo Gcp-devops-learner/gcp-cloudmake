19fe7fc79cf6c6ab1420f57ab5c197f326289eab
formmartin <martin.frenken@formlabs.com>
2023-01-19 16:09:04 -0500

    SD-2156 Terraform fmt

--- formlabs.com/project-factory/projects.tf
+++ formlabs.com/project-factory/projects.tf
@@ -139,7 +139,7 @@ module "packer_windows_machine_test_project" {
   project_name = "packer-windows-test"
   folder_id    = local.folder_ids["devops_dev"]
 
-   activate_apis = [
+  activate_apis = [
     "sourcerepo.googleapis.com",
     "compute.googleapis.com",
     "servicemanagement.googleapis.com",
